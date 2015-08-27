0d471c2ebe3156e8aa068407434c6b4436793b30
Pedro Santos <donbonifacio@gmail.com>
2015-08-27 15:05:42 +0100

    Don't batch urls

--- src/scavenger/core.clj
+++ src/scavenger/core.clj
@@ -18,39 +18,37 @@
       {:response response
        :body (slurp (:body response))})
     (catch Exception e
+      #_(println (.getMessage e))
       {:exception true})))
 
 (defn read-lines [ch batch-size file]
   (go
     (with-open [rdr (clojure.java.io/reader file)]
       (loop [lines []]
-        (if (<= batch-size (count lines))
-          (do (println (str "[read-lines] >! " (count lines)))
-              (>! ch lines)
+        (if-let [line (.readLine rdr)]
+          (do (println (str "[read-lines] >!"))
+              (>! ch line)
               (recur []))
-          (if-let [line (.readLine rdr)]
-            (recur (conj lines line))
-            (do (println (str "[read-lines] >! " (count lines)))
-                (>! ch lines)
-                (println "[read-lines] close!")
-                (close! ch)))))
-      (println "[read-lines] No more urls!"))))
+          (do (println "[read-lines] close!")
+              (close! ch)))))
+      (println "[read-lines] No more urls!")))
 
 (defn get-responses
   "Given a coll of urls, will load the http responses"
   [urls-chan responses-chan]
   (go-loop []
-    (let [urls-batch (<! urls-chan)]
-      (if (not urls-batch)
+    (let [url (<! urls-chan)
+          urls-batch [url]]
+      (if (not url)
         (do
           (println "[get-responses] close!")
           (close! responses-chan))
         (do
-          (println (str "[get-responses] <! " (count urls-batch)))
-          (let [handles (mapv #(http/get (str "http://" %) {:connection-timeout 1000
-                                                            :request-timeout 1000}) urls-batch)
-                responses (mapv make-response handles)]
-            (println (str "[get-responses] >! " (count responses)))
+          (println (str "[get-responses] <! "))
+          (let [handles (mapv #(http/get (str "http://" %) {:connection-timeout 500
+                                                            :request-timeout 500}) urls-batch)
+                responses (time (mapv make-response handles))]
+            (println (str "[get-responses] >! "))
             (>! responses-chan responses))
             (recur))))))
 
@@ -59,7 +57,6 @@
   [ch]
   (go-loop []
     (let [data (<! ch)]
-      (println (str "[reporter] <! " (count data)))
       (if (seq data)
         (recur)
         (do
@@ -68,10 +65,10 @@
 
 (defn -main
   [& args]
-  (let [batch-size 100
-        urls-chan (chan 5)
-        responses-chan (chan 5)
-        file-name "data/alexa1M.txt" ; "data/sample.txt"
+  (let [batch-size 10
+        urls-chan (chan 1)
+        responses-chan (chan 1)
+        file-name "data/sample.txt"
         reader (read-lines urls-chan batch-size file-name)
         responses (get-responses urls-chan responses-chan)]
     (<!! (reporter responses-chan))
